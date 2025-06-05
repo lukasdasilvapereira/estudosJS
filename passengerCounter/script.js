b434cf6a92e6380287a63d11a5443fc9cc7ffaa1
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-06-05 11:28:34 -0300

    .

---
+++ passengerCounter/script.js
@@ -0,0 +1,60 @@
+//document.getElementById("counter").innerText = "5";
+
+//let firstBatch = 5;
+//let secondBatch = 7;
+
+//let count = firstBatch + secondBatch;
+
+//console.log(count);
+
+//1 - Create  a variable, Myage, and set its value to your age and log it to the console.
+
+//let myAge = 19;
+
+//console.log(myAge);
+
+// create two variables , Myage and humandDogRatio and multiply them together, store the result in MyDogAge and log the result to the console.
+
+let myAge = 19;
+let humanDogRatio = 7;
+
+let myDogAge = myAge * humanDogRatio;
+console.log(myDogAge);
+
+// INCREMENTING A VARIABLE
+
+let count = 5;
+
+count = count + 1;
+console.log(count);
+
+// create a variable, bonusPoints, Initialize it to 50, then increase it to 100, then decrement it by 25, then log the final value to the console.
+
+let bonusPoints = 50;
+bonusPoints = bonusPoints + 50; // Increase to 100
+bonusPoints = bonusPoints - 75; // Decrement it to 25
+bonusPoints = bonusPoints + 45; // Increase it to 70
+console.log(bonusPoints); // Log the final value to the console
+
+// UNDERSTANDING FUNCTIONS
+
+// SETTING UP THE RACE
+
+function countdown() {
+    console.log(5);
+    console.log(4);
+    console.log(3);
+    console.log(2);
+    console.log(1);
+}
+
+countdown();
+
+// Create a function that logs out the numbrer 42 to the console
+
+function invoke() {
+    console.log(42);
+}
+
+invoke();
+
