b26aa776e76f8acc7ecdac8d15679b48f94b6bd2
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-06-17 11:27:59 -0300

    .

--- addEventlistener/scripts.js
+++ addEventlistener/scripts.js
@@ -3,3 +3,11 @@ let opened = document.getElementById("open")
 opened.addEventListener("click", function() {
     console.log("I wanna open the box!")
 })
+
+let divContainer = document.querySelector(".container")
+
+divContainer.innerHTML = `<button onclick= 'buy()'>BUY</button>`
+
+function buy() {
+    divContainer.innerHTML += "<p>Thank you for buying</p>"
+}
--- extension/scripts.js
+++ extension/scripts.js
@@ -1,6 +1,26 @@
 let myLeads = []
-const inputEl = document.getElementById("input-el")
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el")
+let inputBtn = document.getElementById("input-btn")
 
-function click() {
-    
+
+inputBtn.addEventListener("click", function () {
+    if (inputEl.value === "") {
+        alert = "Write something"
+    } else {
+        myLeads.push(inputEl.value)
+        renderItems()
+        inputEl.value = ""
+    }
+})
+
+function renderItems() {
+
+    let listItems = "";
+
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li> <a href>" + myLeads[i] + " </a> </li>"
+    }
+
+    ulEl.innerHTML = listItems
 }
