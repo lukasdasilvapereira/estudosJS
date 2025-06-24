17f7030d8da3dcd17276e2ca18c44567d7e7e44f
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-06-24 12:54:06 -0300

    Create index.js

---
+++ jsAssincrono/index.js
@@ -0,0 +1,40 @@
+function multiplicacao(a, b, callback) {
+    let op = a * b
+    console.log(op)
+    callback(a , b)
+}
+
+function soma(a , b){
+    let op = a + b
+    console.log(op)
+}
+
+multiplicacao(2, 4, () => {
+    soma(100, 20)
+})
+
+function entrega(pacote, casa, callback) {
+    console.log(`Aqui está sua ${pacote}, senhor da casa ${casa}`)
+    callback(pacote, casa)
+}
+
+function recebimento(greeting) {
+    console.log(`${greeting}`)
+}
+
+entrega("Pizza", 401, () => {
+    recebimento("Obrigado")
+})
+
+function bomDia(callback) {
+    console.log("Bom dia!")
+    callback()
+}
+
+function boaTarde(boa) {
+    console.log(`${boa}`)
+}
+
+bomDia(() => {
+    boaTarde("Boa tarde!")
+})
